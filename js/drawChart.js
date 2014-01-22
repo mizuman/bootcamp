c0e3305abe26696e96bb6ef87cdbd7a3bbbc71cd
mizuman <YoshikiMiz@gmail.com>
2014-01-23 02:24:15 +0900

    fixed drawChart max width

--- js/drawChart.js
+++ js/drawChart.js
@@ -2,7 +2,7 @@
 // とりあえず、画像を表示
 // 
 var drawChart = function(){
-	var chartWidth = $("#chart").width();
+	var chartWidth = Math.min(1000,$("#chart").width());
 	var url = "http://chart.apis.google.com/chart?chs=" + chartWidth + "x300&chd=t:10,30,60|54,12,87|0,30,70,80|15,54,35,45&cht=lxy&chco=ff0000,0000ff";
 	$("#chart").html("<img src=" + url + ">");
 }
