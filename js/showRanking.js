e6ea06d03863c5641bbd2f547326207cc10915e7
marcy <m.sakurada14@gmail.com>
2014-01-23 15:10:40 +0900

    modified rist display

--- js/showRanking.js
+++ js/showRanking.js
@@ -1,13 +1,41 @@
-
+function ohayou() {
+    //window.alert("Hello!");
+    makeHtml();
+}
 // とりあえず、画像を表示
 // 
+
+
+
+
 var showRanking = function(){
 
-	var query = new Parse.Query("Post");
 	var user = Parse.User.current().get("username");
 
-	query.equalTo("User", user);
-	query.ascending("updatedAt");
+	var mysql = require('mysql');
+	
+	var connection = mysql.createConnection({
+		host     : '153.149.5.92', //接続先ホスト
+		user     : 'thanks',      //ユーザー名
+		password : 'thanksthanks',  //パスワード
+		database : 'user'    //DB名
+	});
+
+
+	//SQL文を書く
+	var sql = 'SELECT * FROM user WHERE UserNO = ?;';
+	//var sql = 'SELECT * FROM user BY LIMIT 0, 10;';
+	//プレースホルダに差し込むデータ
+	var userId = '012345678';
+
+	//接続します
+	connection.connect();
+
+
+	//プレースホルダー使ってSQL発行
+	var query = connection.query(sql, [userId]);
+
+
 	query.find({
 		success:function(results){
 			makeHtml(results);
@@ -15,23 +43,40 @@ var showRanking = function(){
 	});	
 }
 
-var makeHtml = function(results){
+
+
+
+
+//var makeHtml = function(results){
+function makeHtml(){
 	// $("#display").append("hoge");
-	for(var i=0; i < results.length; i++){
-		var entry = results[i];
 
-		var title = entry.get("Position");
-		var image = entry.get("URL");
-		var comment = entry.get("TotalPoint");
-		var user = entry.get("User");
+	//for(var i=0; i < results.length; i++){
+	for(var i=0; i < 5; i++){
+		//var entry = results[i];
 
-		var item = '<tr><th><img src="' + image + '"></th><th>' + user + '</th><th>' + title + '</th><th>' + comment + '</th></tr>';
-		$("#id tbody").append(item);
+		//var image = entry.get("URL");
+		//var name = entry.get("name");
+		//var region = entry.get("region");
+		//var sum_p = entry.get("sum_p");
+		
+		var image = "imageURL";
+		var name = "Name";
+		var region = "Region";
+		var sum_p = 0;
 
-		// var item = 
-
+		var item = '<tr><th><img src="' + image + '"></th><th>' + name + '</th><th>' + region + '</th><th>' + sum_p + '</th></tr>';		
+		
+		$("#tbody").append(item);
 	}
 }
+
+var linkUser = function hoge(code){
+document.f.arg.value=code;
+document.f.submit();
+}
+
+
 
 $(document).ready(function(){
 	showRanking();
